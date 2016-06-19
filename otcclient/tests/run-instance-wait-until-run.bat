03314cfdc4a4ab19a625b440d66bd4bd6b486fe7
zsonagy <gerzsonbigdata@gmail.com>
2016-06-19 14:42:48 +0200

    example changes

--- otcclient/tests/run-instance-wait-until-run.bat
+++ otcclient/tests/run-instance-wait-until-run.bat
@@ -1,1 +1,1 @@
-..\bin\otc ecs run-instances --count 1  --admin-pass yourpass123! --instance-type c1.medium --instance-name instancename --image-name Standard_CentOS_6.7_latest --subnet-name testsubnet --vpc-name testvpc --group-name testsecgroup  --key-name testsshkeypair --file1 /otc/a=/otc/a --wait-instance-running
+..\bin\otc ecs run-instances --count 1  --instance-type c1.medium --instance-name instancename-public --image-name Standard_CentOS_6.7_latest --subnet-name mysubnet --vpc-name myvpc --group-name mytest  --key-name hadoop --file1 /otc/a=/otc/a  --wait-instance-running 

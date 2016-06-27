a43cd3fba0982a879a0287b29959cbb759d9dbcd
zsonagy <gerzsonbigdata@gmail.com>
2016-06-27 14:31:48 +0200

    Add autoscaling test

---
+++ otcclient/tests/test-autoscaling.bat
@@ -0,0 +1,7 @@
+rem get avaliable as groups 
+rem call ..\bin\otc autoscaling describe-auto-scaling-groups 
+rem call ..\bin\otc autoscaling describe-auto-scaling-configuration
+rem call ..\bin\otc autoscaling describe-auto-scaling-instances --auto-scaling-group-name test
+rem call ..\bin\otc autoscaling describe-policies --auto-scaling-group-name test
+rem call ..\bin\otc autoscaling describe-activitylog --auto-scaling-group-name test
+call ..\bin\otc autoscaling describe-quotas

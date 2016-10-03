fe4fe317de905b9732124111bd4136c5401d69dc
Radoslav Kuzma <radoslav.kuzma@t-systems.sk>
2016-10-03 13:31:46 +0200

    Modified shell scripts

--- otcclient/tests-native/5.1_list_backups.sh
+++ otcclient/tests-native/5.1_list_backups.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
-VER=1.1
 
-apitest ()
-{
-    "$@"
-    local status=$?
-    if [ $status -ne 0 ]; then
-        echo "error with $1" >&2
-        echo "$@ => NOT OK " >>"native_test_result_$VER.txt"
-    else
-        echo "$@ => OK " >>"native_test_result_$VER.txt"
-    fi
-    return $status
-
-}
-
-echo "NATIVE TEST $VER" >"native_test_result_$VER.txt"
-
+source otcfunc.sh
 
 
 apitest cinder --insecure backup-list 2>/dev/null

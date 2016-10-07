15fcb23000f5f43901032f26e50a9db0af3b03da
Kamil Stas <kamil.stas@gmail.com>
2016-10-07 11:46:28 +0200

    some modification ecs test scripts

--- otcclient/tests-native/3.15_ecs_resize_server.sh
+++ otcclient/tests-native/3.15_ecs_resize_server.sh
@@ -6,13 +6,18 @@ TEST_SERVER=`nova --insecure list 2>/dev/null | grep TEST_SERVER | awk -F '|' '{
 #list server flavor
 ACT_FLAVOR=`nova --insecure show $TEST_SERVER 2>/dev/null | grep flavor | awk -F '|' '{print $3}' | awk -F '(' '{print $1}'`
 
+NEW_FLAVOR=`nova --insecure flavor-list 2>/dev/null| grep True | awk -F '|' '{print $3}' | sort --random-sort | head -n 1`
 
+echo "........." $TEST_SERVER $ACT_FLAVOR "..............."
 
-	if [ $ACT_FLAVOR = "c1.medium" ];then
-	NEW_FLAVOR="c2.medium"
-		else
-	NEW_FLAVOR="c1.medium"
+check_flavor()
+{
+	if [ "$ACT_FLAVOR" = $NEW_FLAVOR ];then
+	NEW_FLAVOR=`nova --insecure flavor-list 2>/dev/null| grep True | awk -F '|' '{print $3}' | sort --random-sort | head -n 1`
+	check_flavor
 	fi
+}
+
 
 apitest nova --insecure list 2>/dev/null
 echo "Resizing from " $ACT_FLAVOR "to " $NEW_FLAVOR
--- otcclient/tests-native/3.7_ecs_get_flavor_details.sh
+++ otcclient/tests-native/3.7_ecs_get_flavor_details.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 source ./otcfunc.sh
 
-apitest nova --insecure flavor-show c1.medium 2>/dev/null
+FLAVOR=`nova --insecure flavor-list 2>/dev/null| grep True | awk -F '|' '{print $3}' | sort --random-sort | head -n 1`
+
+
+apitest nova --insecure flavor-show $FLAVOR 2>/dev/null

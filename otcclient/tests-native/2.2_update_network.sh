76b12568f77c8760af6d7a411ff8850e92feb44f
Radoslav Kuzma <radoslav.kuzma@t-systems.sk>
2016-10-11 13:34:27 +0200

    Added net-show option

--- otcclient/tests-native/2.2_update_network.sh
+++ otcclient/tests-native/2.2_update_network.sh
@@ -12,4 +12,5 @@ apitest neutron --insecure net-create ${NETWORK_NAME} 2>/dev/null
 NETWORK_ID=`neutron --insecure net-list 2>/dev/null|grep ${NETWORK_NAME}|awk '{print $2}'`
 
 apitest neutron --insecure net-update ${NETWORK_NAME} --name ${NETWORK_NAME_NEW} 2>/dev/null
+apitest neutron --insecure net-show ${NETWORK_NAME_NEW}  2>/dev/null
 apitest neutron --insecure net-delete ${NETWORK_NAME_NEW} 2>/dev/null

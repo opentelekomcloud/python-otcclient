babdbbcf997418faf378263667031e6a15027971
Kamil Stas <kamil.stas@gmail.com>
2016-10-11 15:30:43 +0200

    Signed-off-by: Kamil Stas <kamil.stas@gmail.com>

--- otcclient/tests-native/3.10_ecs_set_list_delete_update_metadata.sh
+++ otcclient/tests-native/3.10_ecs_set_list_delete_update_metadata.sh
@@ -7,5 +7,5 @@ TEST_SERVER=`nova --insecure list 2>/dev/null | grep TEST_SERVER | awk -F '|' '{
 
 apitest nova --insecure meta $TEST_SERVER set newmeta='Update meta data test' 2>/dev/null
 apitest nova --insecure show $TEST_SERVER 2>/dev/null
-#apitest nova --insecure meta $TEST_SERVER delete newmeta  2>/dev/null
+apitest nova --insecure meta $TEST_SERVER delete newmeta  2>/dev/null
 

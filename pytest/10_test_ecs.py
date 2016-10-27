975f7d15b1e7d16d9f847d1afa7890c1e38cb419
Matthias Witte <m.witte@telekom.de>
2016-10-27 16:58:04 +0200

    Add more tests to ecs testing

--- pytest/10_test_ecs.py
+++ pytest/10_test_ecs.py
@@ -11,9 +11,74 @@ def test_ecs_get_token():
     ecs.getIamToken()
     assert OtcConfig.TOKEN != ""
 
-def test_ecs_list_instances():
+def test_ecs_describe_instances():
     OtcConfig.OUTPUT_FORMAT = "noout"
-    instances = json.loads(ecs.describe_instances())
-    assert 'servers' in instances
+    res = json.loads(ecs.describe_instances())
+    assert 'servers' in res
+
+def test_ecs_describe_vpcs():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_vpcs())
+    assert 'vpcs' in res
+
+def test_ecs_describe_addresses():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_addresses())
+    assert 'publicips' in res
+
+def test_ecs_describe_bandwiths():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_bandwiths())
+    assert 'bandwidths' in res
+
+def test_ecs_describe_security_groups():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_security_groups())
+    assert 'security_groups' in res
+
+def test_ecs_describe_subnets():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_subnets())
+    assert 'subnets' in res
+
+def test_ecs_describe_images():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_images())
+    assert 'images' in res
+
+def test_ecs_describe_flavors():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_flavors())
+    assert 'flavors' in res
+
+def test_ecs_describe_key_pairs():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_key_pairs())
+    assert 'keypairs' in res
+
+def test_ecs_describe_volumes():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_volumes())
+    assert 'volumes' in res
+
+def test_ecs_describe_quotas():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_quotas())
+    assert 'absolute' in res
+
+def test_ecs_describe_snapshots():    
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_snapshots())
+    assert 'backups' in res
+
+def test_ecs_describe_network_interfaces(): 
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_network_interfaces())
+    print res
+
+def test_ecs_describe_private_addresses():
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    res = json.loads(ecs.describe_private_addresses())
+    print res
 
 # vim: sts=4 ts=4 sw=4 et:

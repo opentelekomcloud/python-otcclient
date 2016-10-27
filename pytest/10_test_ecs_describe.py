bb281e15bc0aea035c50261432f1ce50b431a66e
Matthias Witte <m.witte@telekom.de>
2016-10-27 18:32:07 +0200

    ecs.describe_* basically covered

--- pytest/10_test_ecs_describe.py
+++ pytest/10_test_ecs_describe.py
@@ -19,6 +19,7 @@ def test_ecs_describe_instances():
 def test_ecs_describe_vpcs():
     OtcConfig.OUTPUT_FORMAT = "noout"
     res = json.loads(ecs.describe_vpcs())
+    VPCS = res
     assert 'vpcs' in res
 
 def test_ecs_describe_addresses():
@@ -71,14 +72,25 @@ def test_ecs_describe_snapshots():
     res = json.loads(ecs.describe_snapshots())
     assert 'backups' in res
 
-def test_ecs_describe_network_interfaces(): 
-    OtcConfig.OUTPUT_FORMAT = "noout"
-    res = json.loads(ecs.describe_network_interfaces())
-    print res
-
 def test_ecs_describe_private_addresses():
     OtcConfig.OUTPUT_FORMAT = "noout"
-    res = json.loads(ecs.describe_private_addresses())
-    print res
+    vpcs = [vpc for vpc in json.loads(ecs.describe_vpcs())['vpcs']]
+    subnets = [sn for sn in json.loads(ecs.describe_subnets())['subnets']]
+    for sn in subnets[0:3]:
+        vpc = [v for v in vpcs if sn['vpc_id'] == v['id']]
+        if len(vpc) == 0:
+            continue
+        OtcConfig.SUBNETNAME = sn['name']
+        OtcConfig.VPCNAME = vpc[0]['name']
+        res = json.loads(ecs.describe_private_addresses())
+        assert 'privateips' in res
+
+def test_ecs_describe_network_interfaces(): 
+    OtcConfig.OUTPUT_FORMAT = "noout"
+    instances = [i for i in json.loads(ecs.describe_instances())['servers']]
+    for i in instances[0:3]:
+        OtcConfig.INSTANCE_NAME = i['name']
+        res = json.loads(ecs.describe_network_interfaces())
+        assert 'interfaceAttachments' in res
 
 # vim: sts=4 ts=4 sw=4 et:

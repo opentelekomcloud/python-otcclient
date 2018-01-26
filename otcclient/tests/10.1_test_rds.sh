6b511b1914f6b11c88036e3171b4ac15a27caf91
zsonagy <gerzsonbigdata@gmail.com>
2018-01-26 21:56:49 +0100

    mrs fix + customrep plugin + test

--- otcclient/tests/10.1_test_rds.sh
+++ otcclient/tests/10.1_test_rds.sh
@@ -3,3 +3,4 @@
 source ./otcclient/tests/otcfunc.sh 
 
 apitest otc rds describe-db-instances
+apitest otc rds create-cluster  --key-name "testkey" --subnet-name "subnet-test" --vpc-name "vpc-test" --group-names "secugroup-test"  --admin-pass "rdspass123!#" --cluster-name "rdstestcluster"
